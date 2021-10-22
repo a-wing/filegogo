7b04feb91c7afb1bbe54393e17e425ac9e9577bb
a-wing <1@233.email>
2021-10-22 08:48:42 +0800

    fix(server): default config

--- cmd/server.go
+++ cmd/server.go
@@ -2,34 +2,37 @@ package cmd
 
 import (
 	"filegogo/server"
+	"filegogo/server/httpd"
 
 	"github.com/BurntSushi/toml"
-	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
 
 func init() {
-	config := &server.Config{}
+	config := &server.Config{
+		Http: &httpd.Config{
+			Listen:    "0.0.0.0:8080",
+			RoomAlive: 1024,
+			RoomCount: 10000,
+		},
+	}
 	app.Commands = append(app.Commands, &cli.Command{
 		Name:  "server",
 		Usage: "websocket broker server",
 		Before: func(c *cli.Context) error {
-			toml.DecodeFile("filegogo-server.toml", config)
+			toml.DecodeFile(c.Path("config"), config)
 			return nil
 		},
 		Action: func(c *cli.Context) error {
-			//log.Println("listen: ", c.String("listen"))
-			log.Println("server")
-			log.Printf("%+v\n", config)
 			server.Run(config)
 			return nil
 		},
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:    "listen",
-				Aliases: []string{"l"},
-				Value:   "0.0.0.0:8033",
-				Usage:   "set server listen address and port",
+				Name:    "config",
+				Aliases: []string{"c"},
+				Value:   "filegogo-server.toml",
+				Usage:   "set config file",
 			},
 		},
 	})
