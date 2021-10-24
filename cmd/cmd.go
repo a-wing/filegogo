d21e76ae58edcf87a11822a63931488e068c0d6e
a-wing <1@233.email>
2021-10-24 10:40:45 +0800

    feat(server): add debug mode

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -9,7 +9,29 @@ import (
 )
 
 var (
-	app = cli.NewApp()
+	app = &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Usage:   "Load configuration from `FILE`",
+			},
+			&cli.BoolFlag{
+				Name:  "debug",
+				Value: false,
+				Usage: "Enabled Debug mode",
+			},
+		},
+		Before: func(c *cli.Context) error {
+			if c.Bool("debug") {
+				log.SetReportCaller(true)
+				log.SetLevel(log.DebugLevel)
+			}
+			log.Warnln(c.Path("config"))
+			log.Debugln(c.Path("config"))
+			return nil
+		},
+	}
 )
 
 func Execute() {
