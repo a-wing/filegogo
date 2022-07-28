66b0c8503fbf2dec317f345b80a06fb76a400dd6
a-wing <1@233.email>
2022-07-28 21:23:25 +0800

    deps(webapp): upgrade vite 2 => vite 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 3000,
     proxy: {
       '/s': 'http://localhost:8080',
       '/config': 'http://localhost:8080',
