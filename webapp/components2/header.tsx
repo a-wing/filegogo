4de879a47e10146eb12b352af084ce747f4ba557
a-wing <1@233.email>
2023-07-06 14:31:41 +0700

    style(webapp): title

--- webapp/components2/header.tsx
+++ webapp/components2/header.tsx
@@ -4,7 +4,7 @@ export default () => {
   return (
     <div className="flex flex-row">
       <img className="h-24" src={ logo } alt="logo" />
-      <h1 className="text-6xl font-serif italic font-bold text-green-300 m-4">Filegogo</h1>
+      <h1 className="text-5xl font-serif italic font-bold text-green-300 m-5">Filegogo</h1>
     </div>
   )
 }
